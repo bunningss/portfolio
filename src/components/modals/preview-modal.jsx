43d59e494c2f7e3671f9b05b22faee3f148cb768
tayef mahmud <mahmudovi183@gmail.com>
2024-12-05 23:41:22 +0600

    disabled preview modal

--- src/components/modals/preview-modal.jsx
+++ src/components/modals/preview-modal.jsx
@@ -6,8 +6,12 @@ export function PreviewModal() {
   const { isOpen, onClose, src } = usePreviewModal();
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} className="w-full md:w-[90vw]">
-      <iframe src={src} frameborder="0" className="w-full h-[90vh]"></iframe>
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      className="w-full md:w-[90vw] h-[70vh]"
+    >
+      <iframe src={src} frameborder="0" className="w-full h-full"></iframe>
     </Modal>
   );
 }
--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -14,26 +14,28 @@ export function ProjectCard({ project }) {
   const { onOpen } = usePreviewModal();
 
   return (
-    <Card onClick={() => onOpen(project?.url)}>
-      <MouseAnimation>
-        <CardContent>
-          <figure className="relative h-[140px]">
-            <Image
-              src={project?.image}
-              alt={project?.title}
-              fill
-              className="object-contain"
-              sizes="300px"
-            />
-          </figure>
-        </CardContent>
-        <CardHeader className="pt-0">
-          <CardTitle className="text-sm">{project?.title}</CardTitle>
-          <CardDescription className="text-xs line-clamp-2">
-            {project?.desc}
-          </CardDescription>
-        </CardHeader>
-      </MouseAnimation>
-    </Card>
+    <a href={project?.url} target="_blank">
+      <Card>
+        <MouseAnimation>
+          <CardContent>
+            <figure className="relative h-[140px]">
+              <Image
+                src={project?.image}
+                alt={project?.title}
+                fill
+                className="object-contain"
+                sizes="300px"
+              />
+            </figure>
+          </CardContent>
+          <CardHeader className="pt-0">
+            <CardTitle className="text-sm">{project?.title}</CardTitle>
+            <CardDescription className="text-xs line-clamp-2">
+              {project?.desc}
+            </CardDescription>
+          </CardHeader>
+        </MouseAnimation>
+      </Card>
+    </a>
   );
 }
